adbc67c278e5d9e2611c8c5a48dafe2cb4218a92
Tom Williams <git@kelvin-williams.dk>
2020-04-15 21:52:23 +0200

    Tests for Triangle getShape()

---
+++ src/test/java/dk/williams/kelvin/triangles/TriangleShapeTest.java
@@ -0,0 +1,40 @@
+package dk.williams.kelvin.triangles;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TriangleShapeTest {
+    @Test
+    public void equilateralTriangle() {
+        Triangle equilateralTriangle = Triangle.of(7.59, 7.59,7.59);
+
+        assertEquals(Triangle.Shape.EQUILATERAL, equilateralTriangle.getShape());
+    }
+
+    @Test
+    public void isoscelesTriangle() {
+        Triangle isoscelesTriangle = Triangle.of(3, 5,3);
+
+        assertEquals(Triangle.Shape.ISOSCELES, isoscelesTriangle.getShape());
+    }
+
+    @Test
+    public void scaleneTriangle() {
+        Triangle scaleneTriangle = Triangle.of(1.001, 1.00100001,1.0009);
+
+        assertEquals(Triangle.Shape.SCALENE, scaleneTriangle.getShape());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void negativeLengthArgumentForTriangle() {
+        Triangle illegalArgument = Triangle.of(-1, -0.4,-1.5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void edgeTooLong() {
+        Triangle illegalArgument = Triangle.of(1, 2, 3);
+    }
+
+
+}
